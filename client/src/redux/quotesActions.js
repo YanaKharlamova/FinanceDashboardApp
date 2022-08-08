a0c3433c46d66d141c6361dbb7b772d3c5284ea1
Yana <petrakharlamowa@gmail.com>
2022-08-09 02:40:25 +0300

    main: all basic functionality for getting quotes from server, made with socketio, redux-thunk

---
+++ client/src/redux/quotesActions.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import {
+    FETCH_QUOTES_REQUEST,
+    FETCH_QUOTES_SUCCESS,
+    FETCH_QUOTES_FAILURE,
+} from "./quotesTypes";
+
+export const fetchQuotes = (quotes) => {
+    return (dispatch) => {
+        dispatch(fetchQuotesRequest());
+        axios
+            .get("http://localhost:4000")
+            .then(() => {
+                dispatch(fetchQuotesSuccess([...quotes]));
+            })
+            .catch(error => {
+                dispatch(fetchQuotesFailure(error.message))
+            })
+    }
+}
+
+export const fetchQuotesRequest = () => {
+    return {
+        type: FETCH_QUOTES_REQUEST
+    }
+}
+
+export const fetchQuotesSuccess = quotes => {
+    return {
+        type: FETCH_QUOTES_SUCCESS,
+        payload: quotes
+    }
+}
+
+export const fetchQuotesFailure = error => {
+    return {
+        type: FETCH_QUOTES_FAILURE,
+        payload: error
+    }
+}
